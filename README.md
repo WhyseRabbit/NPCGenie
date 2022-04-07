79d8010b2d30e6ff1b4d6a462e29987e47d5886f
Alex Yeary <mercy719@gmail.com>
2022-04-07 12:05:47 -0600

    Adjusting project parameters

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NPCGenie
 Pathfinder NPC Generator
+
+Using [this site](https://2e.aonprd.com/Rules.aspx?ID=1040) as guideline for building NPCs.
+
+Data broken into different components
+First names will be broken into three parts and sorted by region and race to maximize name randomness; surnames will be broken into two parts along the same principles.
+
+Ancestry will be given base HP, size (usually Medium or Small), speed, ability boosts (2 += stat), ability flaw (2 -= stat), languages, traits, special features, heritage, heritage feature.
+
+Job/class provides skills and 5 ability boosts (max ability score of 16 or +3 for now). Key ability score will be identified and be max number.
+
+NPCs can be divided into common and adventuring categories. If common, equipment and money will be scarce. If NPC is adventurer, money count will be higher; equipment will be random.
+
+First path to tackle: ancestry and heritage.
--- data.py
+++ data.py
@@ -1,79 +1,106 @@
-from random import randint
+from random import randint, randrange
 
 # Currently, for the sake of saving time, region will be limited to Avistan
 # Using this site as inspiration/template:
 # https://python.plainenglish.io/lets-build-a-random-character-generator-in-python-7f2f42bea5d1
 
-regionalNames = {
-    "Human" : 
-    {
-        "first names" : 
-        {
-            "Absalom" : [],
-            "Andoran" : [],
-            "Brevoy" : [],
-            "Cheliax" : [],
-            "Druma" : [],
-            "Galt" : [],
-            "Hermea" : [],
-            "Hold of the Bekzen" : [],
-            "Irrisen" : [],
-            "Isger" : [],
-            "Lastwall" : [],
-            "Lands of the Linnorm Kings" : [],
-            "Mendev" : [],
-            "Molthune" : [],
-            "Nidal" : [],
-            "Nirmathas" : [],
-            "Numeria" : [],
-            "Qadira" : [],
-            "Razmiran" : [],
-            "Realm of the Mammoth Lords" : [],
-            "The River Kingdoms" : [],
-            "Taldor" : [],
-            "Ustalav" : [],
-            "Varisia" : [],
-            "Worldwound" : []
-        },
-        "Last names" : 
-        {
-            "Absalom" : [],
-            "Andoran" : [],
-            "Brevoy" : [],
-            "Cheliax" : [],
-            "Druma" : [],
-            "Galt" : [],
-            "Hermea" : [],
-            "Hold of Belkzen" : [],
-            "Irrisen" : [],
-            "Isger" : [],
-            "Lastwall" : [],
-            "Lands of the Linnorm Kings" : [],
-            "Mendev" : [],
-            "Molthune" : [],
-            "Nidal" : [],
-            "Nirmathas" : [],
-            "Numeria" : [],
-            "Qadira" : [],
-            "Razmiran" : [],
-            "Realm of the Mammoth Lords" : [],
-            "The River Kingdoms" : [],
-            "Taldor" : [],
-            "Ustalav" : [],
-            "Varisia" : [],
-            "Worldwound" : []
-        },
-        "region" : [
-            "Absalom", "Andoran", "Brevoy", "Cheliax", "Druma", "Galt", "Hermea",
-            "Hold of Belkzen", "Irrisen", "Isger", "Lastwall", "Lands of the Linnorm Kings",
-            "Mendev", "Molthune", "Nidal", "Nirmathas", "Numeria", "Qadira", "Razmiran",
-            "Realm of the Mammoth Lords","The River Kingdomes", "Taldor", "Ustalav",
-            "Varisia", "Worldwound"
+# Seperating names and races as names will be 3 or more parts added together
+
+"""
+To be built from the ground up, this project needs better planning
+"""
+# regionalNames = {
+#     "Human" : 
+#     {
+#         "first names" : 
+#         {
+#             "Absalom" : [],
+#             "Andoran" : [],
+#             "Brevoy" : [],
+#             "Cheliax" : [],
+#             "Druma" : [],
+#             "Galt" : [],
+#             "Hermea" : [],
+#             "Hold of the Bekzen" : [],
+#             "Irrisen" : [],
+#             "Isger" : [],
+#             "Lastwall" : [],
+#             "Lands of the Linnorm Kings" : [],
+#             "Mendev" : [],
+#             "Molthune" : [],
+#             "Nidal" : [],
+#             "Nirmathas" : [],
+#             "Numeria" : [],
+#             "Qadira" : [],
+#             "Razmiran" : [],
+#             "Realm of the Mammoth Lords" : [],
+#             "The River Kingdoms" : [],
+#             "Taldor" : [],
+#             "Ustalav" : [],
+#             "Varisia" : [],
+#             "Worldwound" : []
+#         },
+#         "Last names" : 
+#         {
+#             "Absalom" : [],
+#             "Andoran" : [],
+#             "Brevoy" : [],
+#             "Cheliax" : [],
+#             "Druma" : [],
+#             "Galt" : [],
+#             "Hermea" : [],
+#             "Hold of Belkzen" : [],
+#             "Irrisen" : [],
+#             "Isger" : [],
+#             "Lastwall" : [],
+#             "Lands of the Linnorm Kings" : [],
+#             "Mendev" : [],
+#             "Molthune" : [],
+#             "Nidal" : [],
+#             "Nirmathas" : [],
+#             "Numeria" : [],
+#             "Qadira" : [],
+#             "Razmiran" : [],
+#             "Realm of the Mammoth Lords" : [],
+#             "The River Kingdoms" : [],
+#             "Taldor" : [],
+#             "Ustalav" : [],
+#             "Varisia" : [],
+#             "Worldwound" : []
+#         },
+#         "region" : [
+#             "Absalom", "Andoran", "Brevoy", "Cheliax", "Druma", "Galt", "Hermea",
+#             "Hold of Belkzen", "Irrisen", "Isger", "Lastwall", "Lands of the Linnorm Kings",
+#             "Mendev", "Molthune", "Nidal", "Nirmathas", "Numeria", "Qadira", "Razmiran",
+#             "Realm of the Mammoth Lords","The River Kingdomes", "Taldor", "Ustalav",
+#             "Varisia", "Worldwound"
+#             ],
+#         "age" : randint(7, 95),
+#         "height" : randint(60, 80),
+#         "weight" : randint(120, 160),
+#         "size" : "Medium",
+#         "speed" : 25
+#     }
+# }
+
+
+# classes = {
+#     "Alchemist" : {
+#         "stats" : 
+#     }
+# }
+
+"""
+All stats start at 10 and get increased by the choices generated.
+"""
+
+ancestry = {
+    "Dwarf" : {
+        "heritage" : [
+            "Ancient-Blooded", "Death Warden",
+            "Forge", "Rock", "Strong-Blooded"
             ],
-        "age" : randint(7, 95),
-        "height" : randint(60, 80),
-        "weight" : randint(120, 160),
-        "size" : "Medium",
-        "speed" : 25
+        "HP" : 10,
+        "speed" : 20,
     }
 }
